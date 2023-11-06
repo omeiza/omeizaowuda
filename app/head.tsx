37c3d04732c9406a08d559811d8e90ef3367db1d
Omeiza Owuda <owudaomeiza@gmail.com>
2023-11-06 18:37:18 +0100

    Analytics proper

--- app/head.tsx
+++ app/head.tsx
@@ -1,21 +1,12 @@
 import Script from 'next/script'
+import GoogleAnalytics from "@/app/components/GoogleAnalytics";
 
 export default function Head() {
     return (
         <>
             <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width" />
-            <Script id="google-analytics">
-                {`
-                  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-         
-                  ga('create', 'G-YY5Y67JSHK', 'auto');
-                  ga('send', 'pageview');
-                `}
-            </Script>
+            <GoogleAnalytics />
         </>
     )
 }
